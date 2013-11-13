1e9db1e69b62ccf06995a7070b5cc8562dab1bb5
quocdunginfo <quocdunginfo@gmail.com>
2013-11-13 23:47:15 +0700

    Lam tiep Model

--- application/models/painting/painting_post_model.php
+++ application/models/painting/painting_post_model.php
@@ -2,13 +2,12 @@
 require_once(APPPATH.'/models/post_model.php' );
 class Painting_post_model extends Post_model {
     //extended varible
-    public $art_width=0;
-    public $art_height=0;
-    public $art_sizeunit='cm';
-    public $art_price=0;
-    public $art_sold=0;
-    public $art_id='';
-    
+    public $art_width=0;//=>art_width
+    public $art_height=0;//=>art_height
+    public $art_sizeunit='cm';//=>art_sizeunit
+    public $art_price=0;//=>//=>art_price
+    public $art_sold=0;//=>art_sold
+    public $art_id='';//=>art_id
     //external
     private $cat_material_list=array();
         private $cat_material_list_ready = false;
@@ -20,6 +19,17 @@ class Painting_post_model extends Post_model {
         parent::__construct();
         $this->special=2;
     }
+    //Alias
+    public function get_description()
+    {
+        return $this->content;
+    }
+    //Alias
+    public function set_description($value)
+    {
+        $this->content = $value;
+    }
+    
     public function get_cat_material_list()
     {
         if($this->cat_material_list_ready==true)
@@ -77,14 +87,13 @@ class Painting_post_model extends Post_model {
         foreach($query->result() as $row)
         {
             $this->art_height = $row->art_height;
-            $this->art_width = $row->art_width;
-            $this->art_price = $row->art_price;
-            $this->art_sizeunit = $row->art_sizeunit;
-            $this->art_sold = $row->art_sold;
-            $this->art_id = $row->art_id==''?'RAW'.$this->id:$row->art_id;
+            $this->art_width =$row->art_width;
+            $this->art_price =$row->art_price;
+            $this->art_sizeunit =$row->art_sizeunit;
+            $this->art_sold=$row->art_sold;
+            $this->art_code=$row->art_id==''?'RAW'.$this->id:$row->art_id;
             break;
         }
-        
         self::filter(0);       
     }
     public function get_cat_material_list_text()
@@ -157,149 +166,7 @@ class Painting_post_model extends Post_model {
             $this->db->where('special', $this->special);
             $this->db->update('post', $data);
     }
-    /**
-     * Painting_post_model::get_by_cat()
-     * L?y array post object theo cat_id
-     * 
-     * @param integer $cat_id
-     * @param integer $start_point
-     * @param integer $count
-     * @param integer $active -1(all), 0, 1
-     * @return array obj
-     */
-    function get_by_cat($cat_id=-1, $start_point=0, $count=-1, $active=-1, $special=0)
-    {
-        if($cat_id==-1)
-        {
-            $this->db->select('id');
-            if($special!=-1)
-            {
-                $this->db->where('special',$special);
-            }
-            $this->db->order_by("id", "desc");
-            if($active!=-1) $this->db->where('active',$active);
-            $this->db->distinct();
-            if($count>-1)
-            {
-                if($start_point<0) $start_point=0;
-                $this->db->limit($count, $start_point);
-            }
-            $query = $this->db->get('post');
-            
-            $re = array();
-            if($query->num_rows()<=0) return $re;
-            foreach($query->result() as $row)
-            {
-                array_push($re, $this->get_by_id($row->id));
-            }
-            return $re;
-        }
-        
-        //option 2
-        $this->db->select('post_id');
-        $this->db->order_by("post_id", "desc");
-        $this->db->distinct();
-        if($cat_id!=-1)
-        {
-            $this->db->where('cat_id',$cat_id);
-        }
-        
-        if($count>-1)
-        {
-            if($start_point<0) $start_point=0;
-            $this->db->limit($count, $start_point);
-        }
-        $query = $this->db->get('post_category');
-        
-        $re = array();
-        if($query->num_rows()<=0) return $re;
-        foreach($query->result() as $row)
-        {
-            array_push($re, $this->get_by_id($row->post_id));
-        }
-        return $re;
-    }
-    /**
-     * Painting_post_model::get_by_cat_recursive()
-     * L?y t?t c? post thu?c v? category nào dó (k? c? post thu?c category con)
-     * 
-     * @param integer $cat_id
-     * @param integer $start_point (offset)
-     * @param integer $count (-1: get all, 0, 1, ...)
-     * @param integer $active (-1: ignore, 0, 1)
-     * @param integer $special (-1: ignore, 0, 1, 2, ...)
-     * @return array post_model obj (order by id desc)
-     */
-    public function get_by_cat_recursive($cat_id=-1, $start_point=0, $count=-1, $active=-1, $special=0)
-    {
-        if($cat_id==-1)
-        {
-            return self::get_by_cat($cat_id,$start_point,$count,$active,$special);
-        }
-        //Thuat toan sau chi dung khi post thuoc ve it nhat 1 category
-        //list all child catgory
-        $cat_list = $this->Cat_model->get_cat_tree($cat_id,0,$special);
-        //include current cat if $cat_id!=-1
-        array_push($cat_list,array('id'=>$cat_id));
-        
-        
-        $post_re = array();
-        foreach($cat_list as $cat)
-        {
-            //xet $count cho recusive
-            //if($count>-1 && $count-sizeof($post_re)<=0) break;
-            //$count_tmp = $count;==-1?$count:$count-sizeof($post_re);
-            
-            $post_list = self::get_by_cat($cat['id'],0,-1,$active,$special);
-            //add all post in post_list to post_obj
-            foreach($post_list as $post)
-            {
-                $trung_post_id=false;
-                //dam bao khong bi trung post id trong $post_re do post co the thuoc nhieu cat
-                foreach($post_re as $post_test)
-                {
-                    if($post->id==$post_test->id)
-                    {
-                        $trung_post_id=true;
-                        break;
-                    }
-                }
-                if(!$trung_post_id)
-                {
-                    array_push($post_re,$post);
-                }
-            }
-        }
-        //sort result by id desc
-        for($i=0;$i<sizeof($post_re)-1;$i++)
-        {
-            for($j=sizeof($post_re)-1;$j>=$i;$j--)
-            {
-                if($post_re[$i]->id <= $post_re[$j]->id)
-                {
-                    $tmp = $post_re[$i];
-                    $post_re[$i]=$post_re[$j];
-                    $post_re[$j]=$tmp;
-                }
-            }
-        }
-        
-        $new_post_re=array();
-        //cat post_re theo $count
-        if($count>-1)
-        {
-            for($i=$start_point;$i<$start_point+$count&&$i<sizeof($post_re);$i++)
-            {
-                array_push($new_post_re,$post_re[$i]);
-            }
-            return $new_post_re;
-        }
-        else
-        {
-            return $post_re;
-        }
-        
-    }
+    
     public function filter($direction_in=1)
     {
         if($direction_in==1)
@@ -311,102 +178,11 @@ class Painting_post_model extends Post_model {
         {
             $this->art_price='0'.$this->art_price;
             $this->art_price = number_format($this->art_price,0,'.',',');
-        }
+        }   
+    }
+    public function search($title='', $content_lite='', $art_id='', $art_price_from=0, $art_price_to=0, $art_sold=-1, $active=1, $start_point=0, $count=-1)
+    {
         
-    }
-    public function get_by_multi_cat_recursive($cat_list = array(),$start_point=0,$count=-1,$active=-1,$special=-1)
-    {
-        $post_list = array();
-        foreach($cat_list as $cat_id)
-        {
-            $post_list_tmp = self::get_by_cat_recursive($cat_id,0,-1,$active,$special);
-            //dam bao khong bi trung post id trong $post_re do post co the thuoc nhieu cat
-            foreach($post_list_tmp as $post)
-            {
-                $trung_post_id=false;
-                foreach($post_list as $post_test)
-                {
-                    if($post->id==$post_test->id)
-                    {
-                        $trung_post_id=true;
-                        break;
-                    }
-                }
-                if(!$trung_post_id)
-                {
-                    array_push($post_list,$post);
-                }
-            }
-        }
-        //sort result by id desc
-        for($i=0;$i<sizeof($post_list)-1;$i++)
-        {
-            for($j=sizeof($post_list)-1;$j>=$i;$j--)
-            {
-                if($post_list[$i]->id <= $post_list[$j]->id)
-                {
-                    $tmp = $post_list[$i];
-                    $post_list[$i]=$post_list[$j];
-                    $post_list[$j]=$tmp;
-                }
-            }
-        }
-        
-        $new_post_re=array();
-        //cat post_re theo $count
-        if($count>-1)
-        {
-            for($i=$start_point;$i<$start_point+$count&&$i<sizeof($post_list);$i++)
-            {
-                array_push($new_post_re,$post_list[$i]);
-            }
-            return $new_post_re;
-        }
-        else
-        {
-            return $post_list;
-        }
-    }
-    public function search($title='', $content_lite='', $art_id='', $art_price_from=-1, $art_price_to=-1, $art_sold=0, $start_point=0, $count=-1, $active=1, $special=0)
-    {
-        $this->db->select('id');
-        $this->db->from('post');
-        $this->db->like('title',$title);
-        $this->db->like('content_lite',$content_lite);
-        $this->db->like('art_id',$art_id);
-        if($art_price_from!=-1 && $art_price_to!=-1)
-        {
-            $this->db->where('art_price >= ',$art_price_from);
-            $this->db->where('art_price <= ',$art_price_to);
-        }
-        if($art_sold!=-1)
-        {
-            $this->db->where('art_sold',$art_sold);
-        }
-        if($count>=0)
-        {
-            $this->db->limit($count,$start_point);
-        }
-        if($active!=-1)
-        {
-            $this->db->where('active',$active);
-        }
-        if($special!=-1)
-        {
-            $this->db->where('special',$special);
-        }
-        $this->db->order_by('id','desc');
-        $query = $this->db->get();
-        $re = array();
-        foreach($query->result() as $row)
-        {
-            array_push($re,self::get_by_id($row->id));
-        }
-        return $re;
-    }
-    public function search_count($title='', $content_lite='', $art_id='', $art_price_from=-1, $art_price_to=-1, $art_sold=0, $active=1, $special=0)
-    {
-        return sizeof(self::search($title,$content_lite,$art_id,$art_price_from,$art_price_to,$art_sold,0,-1,$active,$special));
     }
 }
 ?>
